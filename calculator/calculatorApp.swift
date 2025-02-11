00d6917b8c44fa14037ebb9628a83f2e85ec0918
Joseph Massie <Psychovosky@gmail.com>
2025-02-11 14:18:35 -0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Joseph Massie on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
